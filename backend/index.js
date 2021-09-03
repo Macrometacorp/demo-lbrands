6b0602f8d5ba4bb3314961467ded469a97088ea9
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-03 14:07:11 +0530

    reverted deletion of "OPTIONS" handler

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,49 @@ This snippet ties our worker to the router we deifned above, all incoming reques
 are passed to the router where your routes are called and the response is sent.
 */
 addEventListener('fetch', event => {
-    event.respondWith(router.handle(event.request))
+
+    const { request } = event
+    if (request.method === 'OPTIONS') {
+        event.respondWith(handleOptions(request))
+    } else {
+        event.respondWith(router.handle(request))
+    }
 })
+
+function handleOptions(request) {
+    // Make sure the necessary headers are present
+    // for this to be a valid pre-flight request
+    let headers = request.headers
+    if (
+        headers.get('Origin') !== null &&
+        headers.get('Access-Control-Request-Method') !== null &&
+        headers.get('Access-Control-Request-Headers') !== null
+    ) {
+        // Handle CORS pre-flight request.
+        // If you want to check or reject the requested method + headers
+        // you can do that here.
+        let respHeaders = {
+            ...corsHeaders,
+            // Allow all future content Request headers to go back to browser
+            // such as Authorization (Bearer) or X-Client-Name-Version
+            'Access-Control-Allow-Headers': request.headers.get(
+                'Access-Control-Request-Headers'
+            ),
+        }
+
+        return new Response(null, {
+            headers: respHeaders,
+        })
+    } else {
+        // Handle standard OPTIONS request.
+        // If you want to allow other HTTP Methods, you can do that here.
+        return new Response(null, {
+            headers: {
+                Allow: 'GET, HEAD, POST, PUT, DELETE, OPTIONS',
+            },
+        })
+    }
+}
 
 // Create a new router
 const router = Router()
