5ec60ab5dcc15252884aca0a6fa5f1656465ee73
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-07 17:03:37 +0530

    bulk delete

--- dev-helper/uploadImagesToKV.mjs
+++ dev-helper/uploadImagesToKV.mjs
@@ -1,24 +1,66 @@
 #!/usr/bin/env zx
 
-const NAMESPACE_ID = '723596d64b1b43dc876a0c0abbf82e38'
+const NAMESPACE_ID = "723596d64b1b43dc876a0c0abbf82e38";
 
-const CREATE = 'create'
-const DELETE = 'delete'
+const CREATE = "create";
+const DELETE = "delete";
 
-const key = 'automateKey'
-const value = 'images/sample.jpg'
+const value = "images/sample.jpg";
+const pathPrefix = "images/";
 
-const operationType = (argv?._[1] || CREATE).toLowerCase()
-console.log(chalk.cyan(`Received operation as "${operationType}"`))
+const BULK_KV_FILE = "kv_bulk.json";
+
+const operationType = (argv?._[1] || CREATE).toLowerCase();
+console.log(chalk.cyan(`Received operation as "${operationType}"`));
+
+const file = await fs.readFile("./kvpairs.json");
+const data = JSON.parse(file.toString());
 
 if (operationType === CREATE) {
-    console.log(chalk.blue('Starting to upload to KV...'))
-    await $`wrangler kv:key put --namespace-id=${NAMESPACE_ID} "${key}" "${value}" --path`
-    console.log(chalk.green('Upload completed!'))
-} else if(operationType === DELETE) {
-    console.log(chalk.blue('Starting to delete KV...'))
-    await $`wrangler kv:key delete --namespace-id=${NAMESPACE_ID} "${key}"`
-    console.log(chalk.green('Delete completed!'))
+  console.log(chalk.blue("Starting to upload to KV..."));
+
+  let promises = [];
+
+  data.forEach((itemObj) => {
+    const { images } = itemObj;
+    images.forEach((imageObj) => {
+      const { image, name } = imageObj;
+      //   TODO: change "value" to reflect the file name
+      promises.push(
+        $`wrangler kv:key put --namespace-id=${NAMESPACE_ID} "${image}" "${value}" --path`
+      );
+    });
+  });
+
+  await Promise.all(promises);
+  console.log(chalk.green("Upload completed!"));
+} else if (operationType === DELETE) {
+  console.log(chalk.blue("Starting to delete KV..."));
+
+  const deleteKV = [];
+
+  data.forEach((itemObj) => {
+    const { images } = itemObj;
+    images.forEach((imageObj) => {
+      const { image, name } = imageObj;
+      deleteKV.push({
+        key: image,
+        value: "",
+      });
+    });
+  });
+
+  await fs.writeFile(BULK_KV_FILE, JSON.stringify(deleteKV));
+  console.log(chalk.bgBlue("Created bulk delete local file!"));
+  await $`wrangler kv:bulk delete --namespace-id=${NAMESPACE_ID} ${BULK_KV_FILE}`;
+  console.log(chalk.green("Deleted from CF KV"));
+  await fs.unlink(BULK_KV_FILE);
+  console.log(chalk.bgBlue("Deleted bulk delete local file!"));
+  console.log(chalk.green("Delete completed!"));
 } else {
-    console.log(chalk.red(`Unknown operation "${operationType}". Please provide valid operation types.`))
+  console.log(
+    chalk.red(
+      `Unknown operation "${operationType}". Please provide valid operation types.`
+    )
+  );
 }
