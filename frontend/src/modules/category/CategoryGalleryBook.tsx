52da728a66c4e079da55d3c5dde05ccc5a37853c
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-08 16:12:32 +0530

    disable "Add To Cart" button

--- frontend/src/modules/category/CategoryGalleryBook.tsx
+++ frontend/src/modules/category/CategoryGalleryBook.tsx
@@ -4,37 +4,40 @@ import StarRating from "../../common/starRating/StarRating";
 import AddToCart from "../../common/AddToCart";
 import { FashionItem } from "../bestSellers/BestSellerProductRow";
 import { makeBackendUrl } from "../../apiCalls";
+import { LinkContainer } from "react-router-bootstrap";
 
 interface CategoryGalleryBookProps {
   fashionItem: FashionItem;
 }
 
-export class CategoryGalleryBook extends React.Component<
-  CategoryGalleryBookProps
-> {
+export class CategoryGalleryBook extends React.Component<CategoryGalleryBookProps> {
   render() {
     if (!this.props.fashionItem) return;
     return (
       <div className="col-sm-3 col-md-3">
-        <div className="thumbnail no-border">
-          <p className="rating-container">
-            <StarRating stars={this.props.fashionItem.rating} />
-            <span className="pull-right">{`$${this.props.fashionItem.price}`}</span>
-          </p>
-          <img
-            style={{ height: "200px", width: "150px" }}
-            src={makeBackendUrl(`/image/${this.props.fashionItem["_key"]}`)}
-            alt={`${this.props.fashionItem.heading} cover`}
-          />
-          <div className="caption">
-            <h4 className="text-center wrap-text" >{this.props.fashionItem.heading}</h4>
-            <AddToCart
-              fashionItemId={this.props.fashionItem["_key"]}
-              price={this.props.fashionItem.price}
-              variant="center"
+        <LinkContainer to={`/details/${this.props.fashionItem._key}`}>
+          <div className="thumbnail no-border">
+            <p className="rating-container">
+              <StarRating stars={this.props.fashionItem.rating} />
+              <span className="pull-right">{`$${this.props.fashionItem.price}`}</span>
+            </p>
+            <img
+              style={{ height: "200px", width: "150px" }}
+              src={makeBackendUrl(`/image/${this.props.fashionItem["_key"]}`)}
+              alt={`${this.props.fashionItem.heading} cover`}
             />
+            <div className="caption">
+              <h4 className="text-center wrap-text">
+                {this.props.fashionItem.heading}
+              </h4>
+              {/* <AddToCart
+                fashionItemId={this.props.fashionItem["_key"]}
+                price={this.props.fashionItem.price}
+                variant="center"
+              /> */}
+            </div>
           </div>
-        </div>
+        </LinkContainer>
       </div>
     );
   }
