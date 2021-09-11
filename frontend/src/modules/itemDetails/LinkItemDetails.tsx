4acbd9635f04b0f311757e4ddc8c2dfa853ca5ba
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-11 19:21:35 +0530

    LinkedItemDetails cleanup

--- frontend/src/modules/itemDetails/LinkItemDetails.tsx
+++ frontend/src/modules/itemDetails/LinkItemDetails.tsx
@@ -1,24 +1,6 @@
 import React from "react";
-import Modal from "react-modal";
-import { Link } from "react-router-dom";
-
-import LocationOnIcon from "@material-ui/icons/LocationOn";
-import CloseIcon from "@material-ui/icons/Close";
-import SearchIcon from "@material-ui/icons/Search";
 import CachedIcon from "@material-ui/icons/Cached";
-import CancelIcon from "@material-ui/icons/Cancel";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import {
-  Divider,
-  Grid,
-  TextField,
-  List,
-  ListItem,
-  ListItemText,
-  Button,
-} from "@material-ui/core";
-
-import { debounce } from "lodash";
+import { Button } from "@material-ui/core";
 
 import { API, makeBackendUrl } from "../../apiCalls";
 import { FashionItem } from "../bestSellers/BestSellerProductRow";
@@ -32,12 +14,7 @@ const SIZES = ["XS", "S", "M", "L", "XL", "XXL"];
 interface LinkItemDetailsState {
   currentImage: string;
   currentSize: string;
-  fashionItem: FashionItem;
-  quantity: number;
-  isModalOpen: boolean;
-  zipcode: string;
-  zipcodeSuggestion: any;
-  selectedStore: any;
+  fashionItem: any;
 }
 
 interface ImageDetails {
@@ -58,32 +35,15 @@ export class LinkItemDetails extends React.Component<
 
     const { item, promotion, isPromotionSelectionScreenKey } =
       this.props.location.state;
-    const currentImageId = item?._key;
 
     this.promotion = promotion;
     this.isPromotionSelectionScreenKey = isPromotionSelectionScreenKey;
 
     this.state = {
-      currentImage: currentImageId,
+      currentImage: "",
       currentSize: "M",
-      fashionItem: item,
-      quantity: 1,
-      isModalOpen: false,
-      zipcode: "",
-      zipcodeSuggestion: null,
-      selectedStore: null,
+      fashionItem: null,
     };
-
-    this.debouncedSuggestions = debounce(async () => {
-      if (this.state.zipcode) {
-        const key = parseInt(this.state.zipcode[0], 10);
-        if (key === key) {
-          const res = await API.get("suggestion", `/suggestion/${key}`, null);
-          this.setState({ zipcodeSuggestion: res[0] });
-        }
-      }
-    }, 100);
-    this.debouncedSuggestions = this.debouncedSuggestions.bind(this);
   }
 
   async componentDidMount() {
@@ -115,58 +75,24 @@ export class LinkItemDetails extends React.Component<
   render() {
     if (!this.state.fashionItem) return null;
 
-    let promotions;
-    let isLinkedPromotion = false;
-
-    if (this.promotion) {
-      const { type, message, link } = this.promotion;
-      if (type === "price") {
-        promotions = (
-          <>
-            <div>
-              <span className="mr-1">
-                <strong style={{ textDecoration: "line-through" }}>
-                  ${this.state.fashionItem.price}
-                </strong>
-              </span>
-            </div>
-            <div>
-              <strong
-                style={{ color: "#af5071", fontWeight: "bolder", opacity: 0.8 }}
-              >
-                {message}
-              </strong>
-            </div>
-          </>
-        );
-      } else if (type === "link") {
-        isLinkedPromotion = true;
-        promotions = (
-          <>
-            <div>
-              <span className="mr-1">
-                <strong>${this.state.fashionItem.price}</strong>
-              </span>
-            </div>
-            <div>
-              <strong
-                style={{ color: "#af5071", fontWeight: "bolder", opacity: 0.8 }}
-              >
-                {message}
-              </strong>
-            </div>
-          </>
-        );
-      }
-    } else {
-      promotions = (
-        <p>
+    let promotions = (
+      <>
+        <div>
           <span className="mr-1">
-            <strong>${this.state.fashionItem.price}</strong>
+            <strong style={{ textDecoration: "line-through" }}>
+              ${this.state.fashionItem.price}
+            </strong>
           </span>
-        </p>
-      );
-    }
+        </div>
+        <div>
+          <strong
+            style={{ color: "#af5071", fontWeight: "bolder", opacity: 0.8 }}
+          >
+            FREE GIFT!
+          </strong>
+        </div>
+      </>
+    );
 
     return (
       <section className="mb-5">
@@ -229,245 +155,33 @@ export class LinkItemDetails extends React.Component<
                 );
               })}
             </div>
-            {!this.isPromotionSelectionScreenKey && (
-              <div className="detail-quantity">
-                <button
-                  onClick={() => {
-                    if (this.state.quantity > 1) {
-                      this.setState((state) => ({
-                        quantity: state.quantity - 1,
-                      }));
-                    }
-                  }}
-                >
-                  -
-                </button>
-                <div>{this.state.quantity}</div>
-                <button
-                  onClick={() => {
-                    this.setState((state) => ({
-                      quantity: state.quantity + 1,
-                    }));
-                  }}
-                >
-                  +
-                </button>
-              </div>
-            )}
-            {isLinkedPromotion ? (
-              <Button
-                component={Link}
-                style={{
-                  minWidth: "245px",
-                  backgroundColor: "#ef5f96",
-                  color: "#fff",
-                  fontWeight: "bolder",
-                }}
-                variant="contained"
-                to={{
-                  pathname: `/link/${this.promotion.heading}`,
-                  state: {
-                    isPromotionSelectionScreenKey: this.promotion.link,
+            <Button
+              style={{
+                minWidth: "245px",
+                backgroundColor: "#ef5f96",
+                color: "#fff",
+                fontWeight: "bolder",
+              }}
+              variant="contained"
+              onClick={() => {
+                const { price, _key } = this.state.fashionItem;
+                API.post("cart", "/cart", {
+                  body: {
+                    fashionItemId: _key,
+                    // quantity: this.state.quantity,
+                    price: this?.promotion?.price || price,
+                    color: this.getCurrentImageObj()?.name,
+                    size: this.state.currentSize,
                   },
-                }}
-              >
-                Customise your free gift!
-              </Button>
-            ) : (
-              <Button
-                style={{
-                  minWidth: "245px",
-                  backgroundColor: "#ef5f96",
-                  color: "#fff",
-                  fontWeight: "bolder",
-                }}
-                variant="contained"
-                onClick={() => {
-                  const { price, _key } = this.state.fashionItem;
-                  API.post("cart", "/cart", {
-                    body: {
-                      fashionItemId: _key,
-                      quantity: this.state.quantity,
-                      price: this?.promotion?.price || price,
-                      color: this.getCurrentImageObj()?.name,
-                      size: this.state.currentSize,
-                    },
-                  });
-                }}
-              >
-                ADD TO BAG
-              </Button>
-            )}
-
+                });
+              }}
+            >
+              ADD TO BAG
+            </Button>
             <p className="detail-returns">
               <CachedIcon />
               <span>Free returns on all U.S. orders.</span>
             </p>
-            <button
-              type="button"
-              className="detail-location btn btn-light btn-md"
-              onClick={() => {
-                this.setState({ isModalOpen: true });
-              }}
-            >
-              {this.state.selectedStore ? (
-                <div
-                  style={{
-                    display: "flex",
-                    flex: 1,
-                    flexDirection: "row",
-                    fontSize: "10px",
-                    alignItems: "flex-start",
-                    justifyContent: "space-around",
-                  }}
-                >
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <div>{this.state.selectedStore.zipcode}</div>
-                    <div>{this.state.selectedStore.storeName}</div>
-                    <div>{this.state.selectedStore.contact}</div>
-                  </div>
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <span style={{ textDecoration: "underline" }}>
-                      Select Another Store
-                    </span>
-                    {this.isOutOfStock(this.state.selectedStore) && (
-                      <span>Out Of Stock</span>
-                    )}
-                  </div>
-                </div>
-              ) : (
-                !this.isLinkedPromotion && (
-                  <>
-                    <LocationOnIcon style={{ fontSize: "18px" }} />
-                    <strong>Find it near you</strong>
-                  </>
-                )
-              )}
-            </button>
-            <Modal
-              isOpen={this.state.isModalOpen}
-              style={{
-                content: {
-                  top: "50%",
-                  left: "50%",
-                  right: "auto",
-                  bottom: "auto",
-                  marginRight: "-50%",
-                  transform: "translate(-50%, -50%)",
-                },
-              }}
-              contentLabel="Store Availability"
-            >
-              <div className="detail-modal-heading">
-                <h2>Store Availability</h2>
-                <span
-                  onClick={() => {
-                    this.setState({ isModalOpen: false });
-                  }}
-                >
-                  <CloseIcon style={{ fontSize: "24px" }} />
-                </span>
-              </div>
-              <Divider />
-              <Grid container spacing={1} alignItems="flex-end">
-                <Grid item>
-                  <SearchIcon />
-                </Grid>
-                <Grid item>
-                  <TextField
-                    id="input-with-icon-grid"
-                    label="Zip Code"
-                    style={{ width: "350px" }}
-                    onChange={(event) => {
-                      const zipcode = event.target.value;
-                      this.setState({ zipcode }, this.debouncedSuggestions);
-                    }}
-                  />
-                </Grid>
-              </Grid>
-              <List>
-                {this.state.zipcodeSuggestion?.zipcodes.map(
-                  (zipcodeObj: any) => {
-                    const isStoreSelected =
-                      this.state?.selectedStore?.zipcode ===
-                      zipcodeObj?.zipcode;
-                    return (
-                      <ListItem key={zipcodeObj.zipcode}>
-                        <ListItemText
-                          primary={
-                            <div
-                              style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "space-between",
-                                alignItems: "center",
-                              }}
-                            >
-                              <span>
-                                {zipcodeObj.zipcode}, {zipcodeObj.storeName},{" "}
-                                {this.state.zipcodeSuggestion.county}
-                              </span>
-
-                              <div style={{ display: "flex" }}>
-                                <span
-                                  style={{
-                                    paddingRight: "5px",
-                                    color: "rgba(0, 0, 0, 0.54)",
-                                  }}
-                                >
-                                  {this.isOutOfStock(zipcodeObj)
-                                    ? "Out of stock"
-                                    : "Available"}
-                                </span>
-                                {this.isOutOfStock(zipcodeObj) ? (
-                                  <CancelIcon color="disabled" />
-                                ) : (
-                                  <CheckCircleIcon color="disabled" />
-                                )}
-                              </div>
-                            </div>
-                          }
-                          secondary={
-                            <React.Fragment>
-                              <div>{zipcodeObj.contact}</div>
-                              <div>Closes {zipcodeObj.closeTime}</div>
-                              <div
-                                style={{
-                                  display: "flex",
-                                  flexDirection: "row",
-                                  justifyContent: "space-between",
-                                  alignItems: "center",
-                                }}
-                              >
-                                <div style={{ color: "black" }}>
-                                  {zipcodeObj.additionalDetails}
-                                </div>
-                                <Button
-                                  onClick={() => {
-                                    this.setState({
-                                      selectedStore: zipcodeObj,
-                                      isModalOpen: false,
-                                    });
-                                  }}
-                                  variant={
-                                    isStoreSelected ? "contained" : "outlined"
-                                  }
-                                  color="primary"
-                                >
-                                  {isStoreSelected
-                                    ? "Selected"
-                                    : "Select Store"}
-                                </Button>
-                              </div>
-                            </React.Fragment>
-                          }
-                        />
-                      </ListItem>
-                    );
-                  }
-                )}
-              </List>
-            </Modal>
             <h4>Description</h4>
             <p className="detail-description pt-1">
               {this.state.fashionItem.description}
