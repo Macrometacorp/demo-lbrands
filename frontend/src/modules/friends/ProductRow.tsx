7a2df50d1c3288c970df0b5f30c3ee70c038ae93
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-13 12:04:27 +0530

    cursor: pointer for ProductRow

--- frontend/src/modules/friends/ProductRow.tsx
+++ frontend/src/modules/friends/ProductRow.tsx
@@ -39,51 +39,38 @@ export class ProductRow extends React.Component<
     if (!this.state.fashionItem) return null;
 
     return (
-      <div className="white-box">
+      <div className="white-box" style={{ cursor: "pointer" }}>
         <LinkContainer
           to={{
             pathname: `/details/${this.props.fashionItem.heading}`,
             state: { item: this.props.fashionItem },
           }}
         >
-        <div className="media">
-          <div className="media-left media-middle no-padding">
-            <img
-              className="product-thumb border"
-              src={makeBackendUrl(`/image/${this.state.fashionItem["_key"]}`)}
-              alt={`${this.state.fashionItem.heading} cover`}
-            />
-          </div>
-          <div className="media-body  padding-20">
-            <h3 className="media-heading">
-              {this.state.fashionItem.heading}
-              <small className="pull-right ">${this.state.fashionItem.price}</small>
-            </h3>
-            <p className="no-margin">
-              <small>{this.state.fashionItem.category}</small>
-            </p>
-            <FriendRecommendations fashionItemId={this.props.fashionItemId} />
-            <div>
-              <span style={{ display: "block" }}> Rating</span>
-
-                  {/* <span>
-                  <button
-                    className="btn btn-black pull-right"
-                    type="button"
-                  >
-                    View
-                  </button> */}
-                  {/* <AddToCart
-                  fashionItemId={this.state.fashionItem["_key"]}
-                  price={this.state.fashionItem.price}
-                  color={this.state.fashionItem.color}
-                  size={this.state.fashionItem.size}
-                /> */}
-                  {/* </span> */}
+          <div className="media">
+            <div className="media-left media-middle no-padding">
+              <img
+                className="product-thumb border"
+                src={makeBackendUrl(`/image/${this.state.fashionItem["_key"]}`)}
+                alt={`${this.state.fashionItem.heading} cover`}
+              />
             </div>
-            <StarRating stars={this.state.fashionItem.rating} />
+            <div className="media-body  padding-20">
+              <h3 className="media-heading">
+                {this.state.fashionItem.heading}
+                <small className="pull-right ">
+                  ${this.state.fashionItem.price}
+                </small>
+              </h3>
+              <p className="no-margin">
+                <small>{this.state.fashionItem.category}</small>
+              </p>
+              <FriendRecommendations fashionItemId={this.props.fashionItemId} />
+              <div>
+                <span style={{ display: "block" }}> Rating</span>
+              </div>
+              <StarRating stars={this.state.fashionItem.rating} />
+            </div>
           </div>
-        </div>
         </LinkContainer>
       </div>
     );
