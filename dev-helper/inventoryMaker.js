402aec824a6abb16e7d461c38df5fdd514ed75ca
Abhishek Lohani <abhishek.lohani@macrometa.com>
2021-09-15 13:31:32 +0530

    makeInventoryTable helper

---
+++ dev-helper/inventoryMaker.js
@@ -0,0 +1,384 @@
+const zipcodes = [
+  {
+    // Connecticut (CT)	Hartford
+    _key: "0",
+    city: "Hartford, CT",
+    county: "Hartford County",
+    coordinates: "41.77, -72.67",
+    zipcodes: [
+      {
+        zipcode: "06101",
+        storeName: "The Shoppes at Buckland Hills",
+        contact: "(416) 205-1222",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "06102",
+        storeName: "Westfarms VS",
+        contact: "(416) 205-1222",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "06103",
+        storeName: "Westfarms PINK",
+        contact: "(416) 205-1222",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    // Deleware (DE)	Dover
+    _key: "1",
+    city: "Dover, DE",
+    county: "Kent County",
+    coordinates: "39.2, -75.5",
+    zipcodes: [
+      {
+        zipcode: "19901",
+        storeName: "Northfield at Stapleton",
+        contact: "(303) 574-3055",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "19902",
+        storeName: "Dulles Town Center VS",
+        contact: "(703) 444-9690",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "19903",
+        storeName: "Village at Stone Oak",
+        contact: "(210) 481-0137",
+        closeTime: "6:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+    ],
+  },
+  {
+    // District of Columbia (DC)	Washington
+    _key: "2",
+    city: "Washington, DC",
+    county: "District of Columbia",
+    coordinates: "38.91, -77.02",
+    zipcodes: [
+      {
+        zipcode: "20001",
+        storeName: "Southland PINK",
+        contact: "(734) 287-1830",
+        closeTime: "6:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "20002",
+        storeName: "Tyrone Square",
+        contact: "(727) 347-7190",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "20003",
+        storeName: "The Outlet Collection",
+        contact: "(253) 804-0105",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    // Georgia (GA)	Atlanta
+    _key: "3",
+    city: "Atlanta, GA",
+    county: "Fulton County",
+    coordinates: "33.75, -84.39",
+    zipcodes: [
+      {
+        zipcode: "30301",
+        storeName: "Summit VS",
+        contact: "(330) 835-1554",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "30302",
+        storeName: "Houston",
+        contact: "(832) 952-3459",
+        closeTime: "9:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "30303",
+        storeName: "Clackamas Town Center VS",
+        contact: "(503) 652-1752",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+    ],
+  },
+  {
+    // Indiana (IN)	Indianapolis
+    _key: "4",
+    city: "Indianapolis, IN",
+    county: "Marion County",
+    coordinates: "39.78, -86.11",
+    zipcodes: [
+      {
+        zipcode: "46201",
+        storeName: "Twelve Oaks Mall",
+        contact: "(248) 347-0393",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "46202",
+        storeName: "University Park",
+        contact: "(574) 247-4493",
+        closeTime: "6:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "46203",
+        storeName: "Green Acres PINK",
+        contact: "(516) 593-0276",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+    ],
+  },
+  {
+    // Iowa (IA)	Davenport Des Moines
+    _key: "5",
+    city: "Davenport, IA",
+    county: "Scott County",
+    coordinates: "41.52, -90.57",
+    zipcodes: [
+      {
+        zipcode: "52801",
+        storeName: "Apache Mall",
+        contact: "(507) 281-5066",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "52802",
+        storeName: "Somerset Collection",
+        contact: "(248) 816-1683",
+        closeTime: "6:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "52803",
+        storeName: "University Park",
+        contact: "(574) 247-4493",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    // Kansas (KS)	Wichita
+    _key: "6",
+    city: "Wichita, KS",
+    county: "Sedgwick County",
+    coordinates: "37.686, -97.335",
+    zipcodes: [
+      {
+        zipcode: "67201",
+        storeName: "Fremaux Town Center",
+        contact: "(985) 645-0006",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "67202",
+        storeName: "Shadow Lake Towne Center",
+        contact: "(402) 339-1951",
+        closeTime: "7:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "67203",
+        storeName: "Columbia",
+        contact: "(509) 736-3917",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    _key: "7",
+    city: "Tulsa, OK",
+    county: "Tulsa County",
+    coordinates: "36.156, -95.994",
+    zipcodes: [
+      {
+        zipcode: "74101",
+        storeName: "Palm Desert Town PINK",
+        contact: "(760) 568-2346",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "74102",
+        storeName: "Tanger Outlet at Deer Park",
+        contact: "(631) 392-2267",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "74103",
+        storeName: "West Edmonton Mall",
+        contact: "(780) 484-4341",
+        closeTime: "9:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    _key: "8",
+    city: "Logan, UT",
+    county: "Cache County",
+    coordinates: "41.6, -111.7",
+    zipcodes: [
+      {
+        zipcode: "84321",
+        storeName: "Palm Desert Town PINK",
+        contact: "(760) 568-2346",
+        closeTime: "9:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+      {
+        zipcode: "84322",
+        storeName: "Victor Valley Town",
+        contact: "(760) 955-8162",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "84323",
+        storeName: "Masonville",
+        contact: "(519) 645-8969",
+        closeTime: "9:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+    ],
+  },
+  {
+    _key: "9",
+    city: "Bellevue, WA",
+    county: "King County",
+    coordinates: "47.62, -122.21",
+    zipcodes: [
+      {
+        zipcode: "98004",
+        storeName: "Shawnee",
+        contact: "(405) 214-2060",
+        closeTime: "9:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "98005",
+        storeName: "Rogue Valley",
+        contact: "(541) 245-2711",
+        closeTime: "6:00 PM",
+        additionalDetails:
+          "Buy online, pick up in store not available at this location.",
+      },
+      {
+        zipcode: "98006",
+        storeName: "The Outlet Collection",
+        contact: "(253) 804-0105",
+        closeTime: "8:00 PM",
+        additionalDetails:
+          "Open for Customers. Social distancing guidelines throughout the store.",
+      },
+    ],
+  },
+];
+
+const fashionItemKeys = [
+  "BombshellVintageRose.jpeg",
+  "ChantillyLaceEnvious.jpeg",
+  "CottonFleeceFrostedBlueberry.jpeg",
+  "CottonJoggerLinenDahlia.jpeg",
+  "DrapedBackVintageRose.jpeg",
+  "FlowOnPointBerryHeather.jpeg",
+  "FlowOnTealStar.jpeg",
+  "HavanaPushUpPreciousJade.jpeg",
+  "LaceBrazilianDahlia.jpeg",
+  "LaceCheekyBlackPearl.jpeg",
+  "LacePlungeHotPink.jpeg",
+  "LightlyLinedAppleblossomPink.jpeg",
+  "LightweightPimaBrightPinkGraphic.jpeg",
+  "LogoShortPinkStripe.jpeg",
+  "LogoWaistKir.jpeg",
+  "MalibuFabSapphire.jpeg",
+  "MonacoSapphire.jpeg",
+  "PushUpEnvious.jpeg",
+  "PushupPerfectKir.jpeg",
+  "SatinLacePrint.jpeg",
+  "SatinRobeCoconutWhite.jpeg",
+  "SatinShortPinkFizz.jpeg",
+  "ScoopNeckPeony.jpeg",
+  "SeamlessGreen.jpeg",
+  "ShineMontanitaCosmo.jpeg",
+  "ShineStrapCosmo.jpeg",
+  "StretchCottonCrownBlue.jpeg",
+  "StretchFleeceBlue.jpeg",
+  "StretchHiphuggerSoftCrimson.jpeg",
+  "SweatOnEnvious.jpeg",
+  "TahitiScoopOlive.jpeg",
+  "UnlinedSoftFloralMedley.jpeg",
+  "VelourAfricanViolet.jpeg",
+  "WickedOpenLipstick.jpeg",
+  "WirelessLaceBackSageDust.jpeg",
+];
+
+const cleanedCodes = zipcodes.reduce((acc, data) => {
+  data.zipcodes.forEach((detail) => {
+    acc.push(detail.zipcode);
+  });
+  return acc;
+}, []);
+
+const inventoryIems = [];
+
+cleanedCodes.forEach((code) => {
+  fashionItemKeys.forEach((fashionItemKey) => {
+    inventoryIems.push({ _key: `${code}:${fashionItemKey}`, quantity: 1000 });
+  });
+});
+
+console.log(JSON.stringify(inventoryIems));
